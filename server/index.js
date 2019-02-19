b340efcb7918a86d7643bbbdd024edb5ca4fe7c7
Laura Theriot <lauraftheriot@gmail.com>
2019-02-19 14:41:58 -0600

    server set up

---
+++ server/index.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+
+
+// pool.query('SELECT NOW()', (err, res) => {
+//   console.log(err, res)
+//   pool.end()
+// })
+
+const app = express();
+
+app.use(express.static(`${__dirname}/../client/public`)); //Need to change this file path
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
