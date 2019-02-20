4fc9f7f8d1315751f050ac37770ace392f0b19a4
Akin <akinpds@gmail.com>
2019-02-19 20:07:30 -0600

    Added webpack config and client index files

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path =  require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+var SRC_DIR = path.join(__dirname, '/react-client/src');
+var DIST_DIR = path.join(__dirname, '/react-client/dist');
+
+module.exports = {
+    entry: `${SRC_DIR}/index.jsx`,
+    output: {
+        filename: 'bundle.js',
+        path: DIST_DIR
+    },
+    module: {
+        rules: [
+            { test: /\.jsx?/, use: 'babel-loader'},
+            { test: /\.css(js)$/, use: ['style-loader', 'css-loader'] }
+        ]
+    },
+    mode: 'development',
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: 'client/dist/index.html'
+        })
+    ]
+}
