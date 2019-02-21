dbc97c655039fad213c89e2ec9ae5fb8f9db383b
Laura Theriot <lauraftheriot@gmail.com>
2019-02-21 11:19:55 -0600

    Added plantList view. Added file for sample data

---
+++ client/src/components/SampleData.js
@@ -0,0 +1,21 @@
+
+
+const SampleData = [
+  {
+    image: 'https://www.abc.net.au/news/image/10032956-3x2-940x627.jpg',
+    distance: '.01 miles',
+    plant: 'Orange Tree'
+  },
+  {
+    image: 'https://previews.123rf.com/images/xalanx/xalanx1010/xalanx101000124/7999347-apple-trees-in-an-orchard-with-red-apples-ready-for-harvest.jpg',
+    distance: '.03 miles',
+    plant: 'Apple Tree'
+  },
+  {
+    image: 'http://farm4.static.flickr.com/3140/2837642664_9cb3d034dc.jpg',
+    distance: '.06 miles',
+    plant: 'Rosemary Bush'
+  }
+]
+
+export default SampleData;
