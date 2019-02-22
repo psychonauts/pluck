f65c564574c1d683bf1bc5b46af44a80786e5197
Akin <akinpds@gmail.com>
2019-02-22 16:02:13 -0600

    Display map to DOM with direction gui

--- client/src/components/MapView.jsx
+++ client/src/components/MapView.jsx
@@ -2,6 +2,14 @@ import React from 'react';
 import axios from 'axios';
 import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
 import config from '../../../config'
+import mapboxgl from 'mapbox-gl';
+import * as MapboxDirections from '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions';
+
+
+
+var map;
+var directions;
+mapboxgl.accessToken = config.pubKey
 
 // const dotenv = require('dotenv').config();
 
@@ -14,23 +22,43 @@ class MapView extends React.Component {
         super(props)
     }
 
+    componentDidMount() {
+        map = new mapboxgl.Map({
+            container: 'map',
+            style: 'mapbox://styles/mapbox/streets-v11',
+            center: [-98.5795, 39.8283],
+            zoom: 3,
+        });
+        directions = new MapboxDirections({
+            accessToken: config.key,
+            unit: 'metric',
+            profile: 'mapbox/walking'
+        });
+        map.addControl(directions, 'top-left');
+    }
+
     render(){
         return (
-            <Map
-                style="mapbox://styles/mapbox/streets-v9"
-                containerStyle={{
-                    height: "50vh",
-                    width: "50vw",
-                    marginLeft: 'auto',
-                    marginRight: 'auto'
-                }}>
-                <Layer
-                    type="symbol"
-                    id="marker"
-                    layout={{ "icon-image": "marker-15" }}>
-                    <Feature coordinates={[-0.481747846041145, 51.3233379650232]} />
-                </Layer>
-            </Map>
+            <div>
+                <script src='https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-directions/v4.0.0/mapbox-gl-directions.js'></script>
+                <link rel='stylesheet' href='https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-directions/v4.0.0/mapbox-gl-directions.css' type='text/css' />
+                <div id="map" style={{width: 400, height: 300}}/>
+            </div>
+            // <Map
+            //     style="mapbox://styles/mapbox/streets-v9"
+            //     containerStyle={{
+            //         height: "50vh",
+            //         width: "50vw",
+            //         marginLeft: 'auto',
+            //         marginRight: 'auto'
+            //     }}>
+            //     <Layer
+            //         type="symbol"
+            //         id="marker"
+            //         layout={{ "icon-image": "marker-15" }}>
+            //         <Feature coordinates={[-0.481747846041145, 51.3233379650232]} />
+            //     </Layer>
+            // </Map>
         );
     }
 }
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -36,6 +36,7 @@ class App extends React.Component {
     
 
     render() {
+        
         return (
 
                     <div>
@@ -55,7 +56,7 @@ class App extends React.Component {
                         </div>
                              
                         </BrowserRouter>
-                {/* <MapView /> */}
+                <MapView />
                     </div>
 
         );
