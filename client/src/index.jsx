bbf241817169a44959a2bac9ea9c11d090191e75
Akin <akinpds@gmail.com>
2019-02-22 16:53:24 -0600

    Commented out MapView

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -56,7 +56,7 @@ class App extends React.Component {
                         </div>
                              
                         </BrowserRouter>
-                <MapView />
+                {/* <MapView /> */}
                     </div>
 
         );
