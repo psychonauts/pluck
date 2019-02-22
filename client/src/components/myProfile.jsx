36ff468a09c2cba90eec29b0c7c58577ecfb4bd9
Laura Theriot <lauraftheriot@gmail.com>
2019-02-22 17:31:49 -0600

    added user plants to myprofile view dynamically with samle data. images not appearing yet

--- client/src/components/myProfile.jsx
+++ client/src/components/myProfile.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import SampleData from './SampleData.js';
 
 const styles = {
   root: {
@@ -17,6 +23,7 @@ class MyProfile extends React.Component {
     this.state = {
       username: 'myUsername',
       address: '1234 avenue drive, city, state zip',
+      userPlants: SampleData,
     };
   }
 
@@ -39,6 +46,32 @@ class MyProfile extends React.Component {
         >
           {this.state.address}
         </Typography>
+
+        <div className={classes.root}>
+          {this.state.userPlants.map((plant) => {
+            return (
+              <Card className={classes.card}>
+                <CardHeader
+                  title={plant.plant}
+                />
+                <CardMedia
+                  className={classes.media}
+                  image={plant.image}
+                  title={plant.plant}
+                />
+                <CardContent>
+                  <Typography component="p">
+                    {plant.description}
+                  </Typography>
+                </CardContent>
+                <CardActions className={classes.actions} disableActionSpacing>
+                </CardActions>
+              </Card>
+            )
+          })
+          }
+        </div>
+
       </div>
     );
   }
