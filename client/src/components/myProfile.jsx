4dc2cdd22b664c4c365e9d9e565683e13ab1aa28
Laura Theriot <lauraftheriot@gmail.com>
2019-02-22 15:22:42 -0600

    adding username and address to myprofile view. need to make dynamic. need to add users plants

--- client/src/components/myProfile.jsx
+++ client/src/components/myProfile.jsx
@@ -1,11 +1,39 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
 
-const myProfile = props => (
-  <div>
-    <ul>
-      <h5> Username </h5>
-    </ul>
-  </div>
-);
+const styles = {
+  root: {
+    width: '100%',
+    maxWidth: 500,
+    padding: 50,
+  },
+};
 
-export default myProfile;
+function MyProfile(props) {
+  const { classes } = props;
+
+  return (
+    <div className={classes.root}>
+      <Typography
+        variant="h5"
+        gutterBottom
+        // align="center"
+      >
+        USERNAME
+      </Typography>
+
+      <Typography
+        variant="subtitle1"
+        gutterBottom
+        // align="center"
+      >
+        1234 avenue road city, state zipcode
+      </Typography>
+    </div>
+  );
+}
+
+
+export default withStyles(styles)(MyProfile);
