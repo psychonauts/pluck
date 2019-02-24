3307a6e4f8a51a8d8f998888f66614650a5aaf31
zacharybanta <zachary.banta1994@gmail.com>
2019-02-24 05:02:14 -0600

    complete database helpers and server routing

--- database/index.js
+++ database/index.js
@@ -11,8 +11,13 @@ const SENSITIVEDATA = require('./sensitive-data.js'); // this file is git ignore
 
 const connection = mysql.createConnection(SENSITIVEDATA);
 
+// DB HELPERS WORK AS INTENDED
+// all functions are named to explicitly state usage
+
+
+
 // This is a good test to see if we are successfully connected to our database
-const getAllPlants = function(callback) {
+module.exports.getAllPlants = function(callback) {
   connection.query('SELECT * FROM plants', (err, plants) => {
     if (err) {
       callback(err);
@@ -22,6 +27,90 @@ const getAllPlants = function(callback) {
   });
 };
 
+module.exports.getPlantsByGivenZipcode = function(zipcode, callback) {
+  connection.query('SELECT * FROM plants WHERE zipcode = ?', [zipcode], (err, plants) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null, plants);
+    }
+  });
+};
+
+module.exports.getPlantsByGivenUserId = function(userId, callback) {
+  connection.query('SELECT * FROM plants WHERE id_user = ?', [userId], (err, plants) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null, plants);
+    }
+  });
+};
+
+module.exports.addFavorite = function(userId, plantId, callback) {
+  connection.query('INSERT INTO favorites(id_user, id_plant) VALUES(?, ?)', [userId, plantId], (err, favorite) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null, favorite);
+    }
+  });
+};
+
+module.exports.addUser = function(username, hpass, salt, callback) {
+  connection.query('INSERT INTO users(username, hpass, salt) VALUES(?, ?, ?)', [username, hpass, salt], (err, user) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null, user);
+    }
+  });
+};
+
+module.exports.getSaltByGivenUsername = function(username, callback) {
+  connection.query('SELECT salt FROM users WHERE username = ?', [username], (err, salt) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null, salt);
+    }
+  });
+};
+
+module.exports.addPlant = function(userId, title, desc, address, zipcode, imageUrl, callback) {
+  connection.query('INSERT INTO plants(id_user, title, description, address, zipcode, image_url, status) VALUES(?, ?, ?, ?, ?, ?, "show")', [userId, title, desc, address, zipcode, imageUrl], (err, plant) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null, plant);
+    }
+  });
+};
+
+module.exports.getUserIdByGivenUsername = function(username, callback) {
+  connection.query('SELECT id FROM users WHERE username = ?', [username], (err, userId) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null, userId);
+    }
+  });
+};
+
+module.exports.getUserByUsername = function(username, callback) {
+  connection.query('SELECT * FROM users WHERE username = ?', [username], (err, user) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null, user);
+    }
+  });
+};
+
+
+
+// TODO: login----getUser
+
 
 // -------------------------------- TABLE LIST --------------------------------
 //
@@ -30,7 +119,7 @@ const getAllPlants = function(callback) {
 //
 //
 // -- plants
-// -- | id(integer) | id_user(integer) | title(255 max) | description(500 max) | address(255 max) | image_url(255 max) | status('hide', 'show') |
+// -- | id(integer) | id_user(integer) | title(255 max) | description(500 max) | address(255 max) | zipcode(integer) | image_url(255 max) | status('hide', 'show') |
 //
 //
 // -- favorites
