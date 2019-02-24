95c34e2d219a8f43dcae581f48b834f42fcbf091
Laura Theriot <lauraftheriot@gmail.com>
2019-02-23 21:39:22 -0600

    added imgur api key + client id

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
 module.exports = {
-  key: process.env.KEY, 
-  pubKey: 'pk.eyJ1IjoiYXNwMjEzMSIsImEiOiJjanNnOXFxYzcwYnRpNGFuenhtd3hqeG1pIn0.pIcbbrcsRqO9aoCEXjfJzg'
+  key: process.env.KEY,
+  pubKey: 'pk.eyJ1IjoiYXNwMjEzMSIsImEiOiJjanNnOXFxYzcwYnRpNGFuenhtd3hqeG1pIn0.pIcbbrcsRqO9aoCEXjfJzg',
+  clientId: process.env.IMGURCLIENTID,
+  imgurKey: process.env.IMGURKEY,
 };
