2c7498afe1b5f2562dc7e16d6613961429e5259f
cskladz <cskladzien@gmail.com>
2019-02-28 12:15:47 -0600

    linting

--- client/src/components/myProfile.jsx
+++ client/src/components/myProfile.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import SampleData from './SampleData.js';
 
 const styles = {
   root: {
