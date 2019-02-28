73307ee48346111f281c25fda2bbeaede7a78d8f
aeginidae <aeginidae@gmail.com>
2019-02-28 12:06:46 -0600

    add instantSearch config

--- client/src/components/Zip-code.jsx
+++ client/src/components/Zip-code.jsx
@@ -11,6 +11,13 @@ import InputLabel from '@material-ui/core/InputLabel';
 import OutlinedInput from '@material-ui/core/OutlinedInput';
 import { Redirect } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
+import algoliasearch from 'algoliasearch'
+import { InstantSearch } from 'react-instantsearch-dom';
+
+const searchClient = algoliasearch(
+  'S218GIN4YW',
+  '349f0eeaa887cc4df720ebbd1b4dc29a',
+);
 
 const styles = theme => ({
   container: {
